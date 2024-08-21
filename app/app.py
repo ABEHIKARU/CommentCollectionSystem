9439c134f3ff9b82022d16d40ac420b6c6015d70
MATSUO <shinsotsukenshu@nipssc.co.jp>
2024-08-21 17:59:05 +0900

    matsuo

--- app/app.py
+++ app/app.py
@@ -2,10 +2,12 @@ from flask import Flask,render_template
 
 def create_app():
     app=Flask(__name__)
+    
     app.config.from_pyfile('config.py')
 
-    from controller.views1 import hello1_bp
-    app.register_blueprint(hello1_bp,url_prefix='/templates/hello1.html')
+    # Blueprintの登録
+    from controller.search import search_bp
+    app.register_blueprint(search_bp,url_prefix='/templates/hello1.html')
 
     from controller.views2 import hello2_bp
     app.register_blueprint(hello2_bp, url_prefix='/templates/hello2.html')
