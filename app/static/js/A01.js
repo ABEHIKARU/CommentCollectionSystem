93eedc24550db6f5e054005195f672b03bd58254
HONDA <shinsotsukenshu@nipssc.co.jp>
2024-08-26 11:04:11 +0900

    2012年3月6日以降無理修正

--- app/static/js/A01.js
+++ app/static/js/A01.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const endDateInput = document.getElementById('endDate');
 
     const today = new Date().toISOString().split('T')[0];
+    const minStartDate = '2012-03-06';  // 開始日を2012年3月6日に設定
+
+    startDateInput.setAttribute('min', minStartDate);
     startDateInput.setAttribute('max', today);
     endDateInput.setAttribute('max', today);
 
