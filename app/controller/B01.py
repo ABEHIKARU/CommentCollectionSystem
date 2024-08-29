4908fc39fb53ea6e85729ec188e63441ea9e4529
MATSUO <shinsotsukenshu@nipssc.co.jp>
2024-08-29 10:36:50 +0900

    matuo

--- app/controller/B01.py
+++ app/controller/B01.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 b01_bp = Blueprint('b01_bp', __name__)
 
 @b01_bp.route('/B01')
 def show_b01():
+    # 検索セッションチェック
+    
+    
     return render_template('B01.html')
