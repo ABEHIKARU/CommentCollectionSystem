de0f0a6cc102da7d65278027182077b0ba5d0323
MATSUO <shinsotsukenshu@nipssc.co.jp>
2024-08-30 09:01:16 +0900

    松尾

--- app/controller/B01.py
+++ app/controller/B01.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, session
-from google_play_scraper import search
+from google_play_scraper import search,Sort, reviews
+import pandas as pd
+from datetime import date
+
 
 b01_bp = Blueprint('b01_bp', __name__)
 
@@ -18,14 +21,14 @@ def show_b01():
         keyword=session['keyword']
         
         # app_idからアプリ名を特定
-        appName_Search = search(
+        appName_search = search(
             app_id, 
             lang="ja",  # 日本語
             country="jp",  # 日本
             n_hits=1  # 検索件数1件のみ
         )
         # アプリ名のみを取得
-        appName=appName_Search[0]['title']        
+        appName=appName_search[0]['title']        
         
         # ネガポジ種別フラグの変換処理
         if flag==1:
@@ -39,9 +42,28 @@ def show_b01():
         if keyword==None:
             keyword="なし"
             
+        # レビュー1000件抽出
+        reviews_scraping, continuation_token = reviews(
+            app_id, 
+            lang='ja',
+            country='jp',
+            sort=Sort.NEWEST, # 新しい順に抽出
+            count=200, # 1000件抽出(テスト段階では200件)
+        )
+        # データフレームに変換
+        df=pd.DataFrame(reviews_scraping)
+        # レビュー原文と投稿日時のみ格納
+        df_mini=df[['at','content']]
+        # 日付形式の変更
+        df_mini['at'] = pd.to_datetime(df_mini['at']).dt.strftime('%Y/%m/%d %H:%M')    
+        
+        # 終了日探索
+        
+        
+        
         # B01.htmlへ遷移            
         return render_template('B01.html',appName=appName,start_date=start_date,end_date=end_date,sentiment=sentiment,keyword=keyword)
     else:
     # セッションに値がセットされていない場合の処理
         errorMessage="データの取得に失敗しました"
-        return render_template('A01.html')
+        return render_template('A01.html')    
