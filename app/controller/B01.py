4a706127e36530064e05b68231cdd5be9431e04e
MATSUO <shinsotsukenshu@nipssc.co.jp>
2024-09-04 11:32:26 +0900

    松尾

--- app/controller/B01.py
+++ app/controller/B01.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, session
 from google_play_scraper import search, Sort, reviews
 import pandas as pd
-from controller.B02 import filter_reviews_by_sentiment  # B02からフィルタリング関数をインポート
+# from controller.B02 import filter_reviews_by_sentiment  # B02からフィルタリング関数をインポート
 
 b01_bp = Blueprint('b01_bp', __name__)
 
@@ -41,10 +41,10 @@ def show_b01():
     print(df_reviews)
     
     # ネガポジフィルタリング
-    filtered_reviews = filter_reviews_by_sentiment(df_reviews, sentiment)
+    # filtered_reviews = filter_reviews_by_sentiment(df_reviews, sentiment)
 
     # データが存在する場合としない場合での分岐
-    if not filtered_reviews.empty:
+    if not df_reviews.empty:
         return render_template('B01.html', appName=appName, start_date=start_date, end_date=end_date, sentiment=sentiment, keyword=keyword)
     else:
         errorMessage_list = "条件に一致するレビューが見つかりませんでした"
@@ -82,6 +82,7 @@ def scraping_reviews(app_id, end_date_search,start_date_search,keyword):
     # 変数の初期化
     df_M = pd.DataFrame()
     df_S = pd.DataFrame()
+    l=0
     continuation_token = None
     
     while True:
@@ -100,7 +101,7 @@ def scraping_reviews(app_id, end_date_search,start_date_search,keyword):
         
         # レビューが存在しない場合
         if not result:
-            break
+            return df_M
 
         # 抽出した1000件のデータを継ぎ足す
         df_L = pd.DataFrame(result)
@@ -119,40 +120,58 @@ def scraping_reviews(app_id, end_date_search,start_date_search,keyword):
         if (df_M['at']>=start_date_search).any():
             df_M = df_M[df_M['at'] >= start_date_search]
             start_date_flag=True
+        
+        # 開始日がない、かつ、21件未満の場合
+        if start_date_flag==False and df_M.shape[0]<21:
+            continue
+        
+        # 変数の初期化(1度だけ実行)
+        if l==0:
+            i=0
+            j=21  
+        while True:
+            # 21件を別のdfに継ぎ足す
+            df_S=pd.concat([df_S,df_M[i:j]],ignore_index=True)
             
-            if df_M.shape[0]>=21 or (end_date_flag==True and start_date_flag==True): # TODO:21はあとで変数に変える
-                # 過去21件のレビューを新しいデータフレームに格納
-                df_S = df_M[0:21] # TODO:21はあとで変数に変える
-                
+            # 次の21件がある場合、内側のループを抜ける
+            if df_S[i:j].shape[0]==21:
                 break
-            
+            # 次の21件がない場合、内側のループを抜け、外側のループの最初の処理に戻る
+            if df_S[i:j].shape[0]==21:
+                break
+            continue
         
-            # # 21件未満の場合
-            # if df_M.shape[0]<21:
-            #     continue
-            # # 0件の場合
-            # elif df_M.shape[0]:
-            #     break     
-        
-        
-        
-        # # 詳細設計書 b-(b)
-        # # while True:
-        #     # データが21件以上ある場合
-        #     if df_M.shape[0]>=21 or (end_date_flag==True and start_date_flag==True): # TODO:21はあとで変数に変える
-        #         # 過去21件のレビューを新しいデータフレームに格納
-        #         df_S = df_M[0:21] # TODO:21はあとで変数に変える
-                
-        #         # # キーワードが指定されている場合
-        #         # if keyword != 'なし':
-        #         #     df_S = df_S[df_S['content'].str.contains(keyword, case=False, na=False)]
-            
-        #         #     # キーワードフィルタリングの結果、データが0件になった場合
-        #         #     if df_S.empty:
-        #         #         continue
+        # キーワード指定がされている場合
+        if keyword != 'なし':
+            df_S = df_S[df_S['content'].str.contains(keyword, case=False, na=False)]
 
-        #         # 日付形式の変更
-        #         df_S['at'] = df_S['at'].dt.strftime('%Y/%m/%d %H:%M')
-                            
-        #         break
+            # キーワードフィルタリングの結果、データが0件になった場合、データを補充する(21件を超えないようにする)
+            if df_S.empty:
+                i+=21
+                j+=21
+                # 外側のループに戻りたい；；
+                continue
+                
+                
+                
+                
+                
+                
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+
+        # 日付形式の変更
+        df_M['at'] = df_S['at'].dt.strftime('%Y/%m/%d %H:%M')
+                    
+        break
+    
     return df_S
