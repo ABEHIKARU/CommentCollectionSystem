39ec582adca4c36e7ece453a8fd8ebf87b2d282d
MATSUO <shinsotsukenshu@nipssc.co.jp>
2024-09-06 09:06:29 +0900

    matuo

--- app/controller/B01.py
+++ app/controller/B01.py
@@ -39,15 +39,16 @@ def show_b01():
     start_date_search = pd.to_datetime(start_date)
     df_reviews = scraping_reviews(app_id, end_date_search,start_date_search,keyword)
     
-    # pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_rows', None)
     # pd.set_option('display.max_columns', None)
     # pd.options.display.max_colwidth=5000
     print(df_reviews)
     
     # ネガポジフィルタリング
     filtered_reviews = filter_reviews_by_sentiment(df_reviews, sentiment)
-    filtered_reviews =process_reviews(filtered_reviews)
+    print(filtered_reviews)
     
+    filtered_reviews =process_reviews(filtered_reviews)
     print(filtered_reviews)
     
     # データが存在する場合としない場合での分岐
